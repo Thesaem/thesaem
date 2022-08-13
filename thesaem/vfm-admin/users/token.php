db20c5a395a673909df45ced49196e61d72bc287
Thesaem <dicky@thesaemcosmetic.co.id>
2022-08-14 02:56:27 +0700

    first upload

---
+++ thesaem/vfm-admin/users/token.php
@@ -0,0 +1,9 @@
+<?php
+
+ $_TOKENS = array (
+  'dicky@thesaemcosmetic.co.id' => 
+  array (
+    'token' => '672858cad525d8d6f0cc8d5aecac232077c9ef7c',
+    'birth' => 1660231198,
+  ),
+);
---
+++ thesaem/vfm-admin/users/users.php
@@ -0,0 +1,31 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$6qASHC6U$YeoF91I8fR/Wy.ilU5sCR/',
+    'role' => 'superadmin',
+  ),
+  1 => 
+  array (
+    'name' => 'dicky',
+    'pass' => '$1$cWCU3Mai$6aZRVe0g6q42QIvUPgIv50',
+    'role' => 'admin',
+    'email' => 'dicky@thesaemcosmetic.co.id',
+  ),
+  2 => 
+  array (
+    'name' => 'thesaem',
+    'pass' => '$1$gtZCfpMG$YDO4znyc9Zyt3Y2v6WgcY/',
+    'role' => 'admin',
+    'email' => 'server.agungjaya@gmail.com',
+  ),
+  3 => 
+  array (
+    'name' => 'Nasya',
+    'pass' => '$1$CbVX19qA$rNvaexNjqEJj4oQxt/3dZ0',
+    'role' => 'user',
+    'email' => 'nasya@ptagung.co.id',
+  ),
+);
